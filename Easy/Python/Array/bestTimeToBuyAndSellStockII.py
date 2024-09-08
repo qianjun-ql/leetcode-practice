4824dd94084526588575d525c4605401d47dc0dd
QianjunL <qianjunliang.ql@gmail.com>
2024-09-08 11:44:24 -0600

    update

---
+++ Easy/Python/Array/bestTimeToBuyAndSellStockII.py
@@ -0,0 +1,43 @@
+# You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+# On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+# Find and return the maximum profit you can achieve.
+
+ 
+
+# Example 1:
+
+# Input: prices = [7,1,5,3,6,4]
+# Output: 7
+# Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+# Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+# Total profit is 4 + 3 = 7.
+# Example 2:
+
+# Input: prices = [1,2,3,4,5]
+# Output: 4
+# Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+# Total profit is 4.
+# Example 3:
+
+# Input: prices = [7,6,4,3,1]
+# Output: 0
+# Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+ 
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        nonZeroIndex = 0
+        
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[nonZeroIndex] = nums[i]
+                nonZeroIndex += 1
+                
+        for i in range(nonZeroIndex, len(nums)):
+            nums[i] = 0
